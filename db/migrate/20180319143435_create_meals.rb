2f4d9f78c009415d2ca5bd89faf2b02782837886
MaximeRoberti <maxime.roberti@gmail.com>
2018-03-19 16:11:15 +0100

    Migration problem solved

--- db/migrate/20180319143435_create_meals.rb
+++ db/migrate/20180319143435_create_meals.rb
@@ -4,7 +4,7 @@ class CreateMeals < ActiveRecord::Migration[5.1]
       t.string :name
       t.string :description
       t.float :price
-      t.references :MealProvider, foreign_key: true
+      t.references :meal_provider, foreign_key: true
 
       t.timestamps
     end
