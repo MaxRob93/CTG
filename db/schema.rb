d537d204efd9e91da1eb794908b196bf5695b24d
Diego Gilon <diego.gilon@gmail.com>
2018-03-20 14:06:59 +0100

    nesting of routes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180320102921) do
+ActiveRecord::Schema.define(version: 20180320121629) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,8 @@ ActiveRecord::Schema.define(version: 20180320102921) do
     t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_meal_providers_on_user_id"
   end
 
   create_table "meals", force: :cascade do |t|
