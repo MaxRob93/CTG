0c21b7e30811d5cbea618d71691da543d803a20a
Hadrien Robert <robert.hadrien@gmail.com>
2018-03-21 18:07:17 +0100

    elkjre

--- app/models/meal_provider.rb
+++ app/models/meal_provider.rb
@@ -4,11 +4,7 @@ class MealProvider < ApplicationRecord
   validates :name, presence: true
   validates :address, presence: true
   validates :category, presence: true
-<<<<<<< HEAD
-
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
-=======
   mount_uploader :photo, PhotoUploader
->>>>>>> cf9f03087f15f9d6fa05afb19a424538c8f58740
 end
