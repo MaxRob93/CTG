de56999fe4c8dbe422149a658e93196864f395c7
MaximeRoberti <maxime.roberti@gmail.com>
2018-03-23 15:56:34 +0100

    Modified order page

--- db/schema.rb
+++ db/schema.rb
@@ -31,9 +31,9 @@ ActiveRecord::Schema.define(version: 20180322204624) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.string "photo"
     t.float "latitude"
     t.float "longitude"
-    t.string "photo"
     t.index ["user_id"], name: "index_meal_providers_on_user_id"
   end
 
